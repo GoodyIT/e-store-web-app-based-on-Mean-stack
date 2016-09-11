04697f53e5bebf92d618357a93377d3fe23a1748
AhmedAli7O1 <ahmedaliabdullah70@gmail.com>
2016-09-11 19:30:40 +0200

    update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
     stylish = require('jshint-stylish'),
     uglify = require('gulp-uglify'),
     usemin = require('gulp-usemin'),
+    sourcemaps = require('gulp-sourcemaps'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
@@ -14,12 +15,19 @@ var gulp = require('gulp'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
     ngannotate = require('gulp-ng-annotate'),
-    del = require('del');
+    del = require('del'),
+    browserSync = require('browser-sync').create();
 
 
+
+gulp.task('browser-sync', ['server'], function() {
+    return browserSync.init({
+        proxy: "localhost:4000"
+    });
+});
 
 gulp.task('favicon', function() {
-    gulp.src('client/app/favicon.ico').pipe(gulp.dest('server/public'));
+    return gulp.src('client/app/favicon.ico').pipe(gulp.dest('server/public'));
 });
 
 gulp.task('debug', function(){
@@ -29,19 +37,58 @@ gulp.task('debug', function(){
 });
 
 // Images
-gulp.task('imagemin', ['favicon'], function() {
-  return del(['server/public/images']), gulp.src('client/app/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('server/public/images'));
+gulp.task('imagemin', ['favicon'], function () {
+    
+    return del(['server/public/images']),
+    
+    gulp.src('client/app/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('server/public/images'));
+
+});
+
+gulp.task('usemin-vendors', ['debug'], function () {
+     
+     return del(['server/public/scripts/vendors.js', 'server/public/styles/vendors.css']),
+
+     gulp.src('client/app/**/*.html')
+        .pipe(usemin({
+            css: [minifyCss(), rev()],
+            css_vendors: [minifyCss(), rev()],
+            js_vendors: [
+                sourcemaps.init({loadMaps: true}),
+                ngannotate(), 
+                'concat',
+                uglify(),
+                sourcemaps.write('./')
+            ],
+            js: [
+                sourcemaps.init({loadMaps: true}),
+                ngannotate(), 
+                'concat',
+                uglify(),
+                sourcemaps.write('./')
+            ]
+        }))
+        .pipe(gulp.dest('server/public'));
+
 });
 
 gulp.task('usemin', ['debug'], function () {
-    return del(['server/public/scripts', 'server/public/styles']), gulp.src('client/app/**/*.html')
+    del(['server/public/scripts/main*.js', 'server/public/styles/main*.css']), gulp.src('client/app/**/*.html')
         .pipe(usemin({
             css: [minifyCss(), rev()],
-            js: [ngannotate(), uglify(), rev()]
+            js: [
+                sourcemaps.init({loadMaps: true}),
+                ngannotate(), 
+                'concat',
+                uglify(), 
+                rev(),
+                sourcemaps.write('./')
+            ]
         }))
-        .pipe(gulp.dest('server/public'));
+        .pipe(gulp.dest('server/public'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('clean',function () {
@@ -55,23 +102,25 @@ gulp.task('copyfonts', ['clean'], function () {
         .pipe(gulp.dest('server/public/fonts'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browser-sync'], function () {
     gulp.watch('client/app/**/*', ['usemin']);
 });
 
-gulp.task('default', ['clean'], function () {
-    gulp.start('imagemin', 'usemin', 'copyfonts');
-});
+gulp.task('default', ['clean', 'copyfonts', 'imagemin', 'usemin-vendors']);
 
-gulp.task('server' , function(){
+gulp.task('server' , ['default'], function(cb){
+    
     nodemon({
         script: 'server/bin/www',
         ignore: [
             'node_modules/',
             'client/',
             'server/public/'
-        ]
+        ],
+    }).on('start', function(){
+        cb(null);
     });
+
 });
 
-gulp.task('serve', ['default', 'watch','server']);
+gulp.task('serve', ['watch']);
